c1fd41eaa24b539b183369fd5eaec09a9865c7b7
jihaad2021 <jihaadariefpangestu@gmail.com>
2023-10-11 01:05:35 +0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Jihaad2021',
+    author_email= 'jihaadariefpangestu@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
