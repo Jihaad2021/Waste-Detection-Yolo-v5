7937ebcc39685307a140e5884ed14ccfcedd996c
jihaad2021 <jihaadariefpangestu@gmail.com>
2023-10-12 04:49:47 +0700

    logger, exception, and utils added

---
+++ app.py
@@ -0,0 +1,13 @@
+from wasteDetection.logger import logging
+from wasteDetection.exception import AppException
+import sys
+
+# to check logging working or not 
+#logging.info("welcome to my custom log")
+
+# to check exception 
+#try: 
+#    a = 3 / "s"
+    
+#except Exception as e:
+#    raise AppException(e, sys)
